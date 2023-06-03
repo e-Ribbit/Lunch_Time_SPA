f8241defc700ed0e0e2280ce6d6da403e7a507f5
e-Ribbit <jstecode@gmail.com>
2023-06-03 13:52:28 +0200

    define paths

--- Lunch_Time/src/app/app-routing.module.ts
+++ Lunch_Time/src/app/app-routing.module.ts
@@ -3,14 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormPlaceOrderComponent } from './form-place-order/form-place-order.component';
 import { FormCreateOrderComponent } from './form-create-order/form-create-order.component';
 import { FormDataComponent } from './form-data/form-data.component';
+import { EditFormDataComponent } from './edit-form-data/edit-form-data.component';
 import { MainListComponent } from './main-list/main-list.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'place-order', component: FormPlaceOrderComponent },
+  { path: 'create-order', component: FormCreateOrderComponent },
+  { path: 'form-data', component: FormDataComponent },
+  { path: 'edit-form-data', component: EditFormDataComponent },
+  { path: 'main-list', component: MainListComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
